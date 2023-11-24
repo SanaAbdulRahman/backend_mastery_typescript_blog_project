851f23c2660bd7ff825377255c63472c91740d52
Sanath <sanaabdulmanaf88@gmail.com>
2023-11-24 11:14:49 +0400

    basic project setup for typescript and nodejs

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import db from "./models";
+const app = express();
+const port = process.env.PORT || 3000;
+
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`App running on ${port}`);
+    })
+})
